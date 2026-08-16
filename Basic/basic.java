f895bf8af1409da0b6991b4e5dd054c18e5263ae
Prashant Ray <prashantray878@gmai.com>
2026-08-16 19:43:05 +0530

    basic of java

---
+++ Basic/basic.java
@@ -0,0 +1,10 @@
+public class basic {
+public static void main(String[]args) {
+    System.out.println("hello world");  
+    System.out.println("hello prashant"); 
+    System.out.println("This is my first java code");
+    } 
+    
+}
+    
+
